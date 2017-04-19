1dd03310d8e73aa2cf1ff93a6837dc566b638bf5
yourUsername <your_email@example.com>
2017-04-19 02:17:55 -0400

    Training In class practice

--- bin/lab.rb
+++ bin/lab.rb
@@ -3,20 +3,29 @@
 working_array = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
 # Assign `20` to the end of the array using the array's length.
-
+working_array[working_array.length -1] = 20
+puts working_array
 # Access the 3rd element from the end of the array.
-
+working_array[-3]
+puts working_array
 # Access element 9 from the array
+working_array[8]
+puts working_array
+# Assign `[-12, -49]` to the 5th element from the end. (you will be placing this array inside of the working_array)
+working_array[-5] = [-12, -49]
 
-# Assign `[-12, -49]` to the 5th element from the end.
+# my original wrong answer:
+# working_array.push[-5] << -12
 
 # Access all the elements starting at index 1.
-
+working_array[1, working_array.length]
+puts working_array
 ## Bonus ##
 # In bin/lab.rb use a block initializer with Array.new to create
 # a Ruby Array with ten elements where elements are equal to their
 # index multiplied by 2. Store the result and display it on the
 # console with puts <array name>.
+new_array = Array.new(10) { |i| (i*2)}
 
 # binding.pry
 # ""
---
+++ bin/story.rb
@@ -0,0 +1,12 @@
+peeps = ["Kelly", "Cari", "Mel"]
+
+puts "There once were three friends, #{peeps[0]}, #{peeps[1]}, and #{peeps[2]}."
+puts "#{peeps[0]}, was from Maryland, #{peeps[1]} was from Australia, and "
+peeps << "Taylor"
+
+puts "#{characters[-2]} befriended #{characters[-1]}, #{characters[0]}\'s known enemy.
+      #{characters[0]} could no longer be their friend."
+
+characters.shift
+
+puts "#{characters[0]}, #{characters[1]}, and #{characters[2]} needed to think of lunch plans."
